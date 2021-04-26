0f3ce1a79e391528f1aed5a83908038d1cc33cd0
tkubrak <taras.kubrak@dataart.com>
2021-04-26 10:53:24 +0300

    feat: generate fake data

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -3,14 +3,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 abstract class BaseFixture extends Fixture
 {
+    /** @var Generator */
+    protected $faker;
+
     private $manager;
 
     public function load(ObjectManager $manager)
     {
         $this->manager = $manager;
+        $this->faker = Factory::create();
 
         $this->loadData($manager);
     }
